ea3c6b5dac44cd49e8439af0ac9a3e589d92aa28
Deividas Balandis <deividas.balandis@gmail.com>
2020-03-11 15:44:05 +0200

    Initial commit

---
+++ src/main/java/com/nanoDATA/NanoDataApplication.java
@@ -0,0 +1,13 @@
+package com.nanoDATA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanoDATA/NanoDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanoDATA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
